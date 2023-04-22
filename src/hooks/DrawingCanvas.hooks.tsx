cb14d16d54573becb74bc92dfc0baf3cdc3a93c3
Lucca Paradeda <lucca.paradeda@edu.pucrs.br>
2023-04-22 03:28:45 -0300

    feat: drwaingCanvas

---
+++ src/hooks/DrawingCanvas.hooks.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useRef } from 'react';
+
+export function useOnDraw(onDraw: any) {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const isDrawingRef = useRef(false);
+  const prevPointRef = useRef<any | null>(null);
+
+  const mouseMoveListenerRef = useRef<any | null>(null);
+  const mouseUpListenerRef = useRef<any | null>(null);
+
+  function setCanvasRef(ref: HTMLCanvasElement | null) {
+    canvasRef.current = ref;
+  }
+
+  function onCanvasMouseDown() {
+    isDrawingRef.current = true;
+  }
+
+  useEffect(() => {
+    function computePointInCanvas(clientX: number, clientY: number) {
+      if (canvasRef.current) {
+        const boundingRect = canvasRef.current.getBoundingClientRect();
+        return {
+          x: clientX - boundingRect.left,
+          y: clientY - boundingRect.top,
+        };
+      } else {
+        return null;
+      }
+    }
+    function initMouseMoveListener() {
+      const mouseMoveListener = (e: MouseEvent) => {
+        if (isDrawingRef.current && canvasRef.current) {
+          const point = computePointInCanvas(e.clientX, e.clientY);
+          const ctx = canvasRef.current.getContext('2d');
+          if (onDraw) onDraw(ctx, point, prevPointRef.current);
+          prevPointRef.current = point;
+        }
+      };
+      mouseMoveListenerRef.current = mouseMoveListener;
+      window.addEventListener('mousemove', mouseMoveListener);
+    }
+
+    function initMouseUpListener() {
+      const listener = () => {
+        isDrawingRef.current = false;
+        prevPointRef.current = null;
+      };
+      mouseUpListenerRef.current = listener;
+      window.addEventListener('mouseup', listener);
+    }
+
+    function cleanup() {
+      if (mouseMoveListenerRef.current) {
+        window.removeEventListener('mousemove', mouseMoveListenerRef.current);
+      }
+      if (mouseUpListenerRef.current) {
+        window.removeEventListener('mouseup', mouseUpListenerRef.current);
+      }
+    }
+
+    initMouseMoveListener();
+    initMouseUpListener();
+    return () => cleanup();
+  }, [onDraw]);
+
+  return {
+    setCanvasRef,
+    onCanvasMouseDown,
+  };
+}
