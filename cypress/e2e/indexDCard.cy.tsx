abb9e4a60eb2b488d5bc1b6303fbc4ac179bef9e
Lucca Paradeda <lucca.paradeda@edu.pucrs.br>
2023-04-24 00:54:43 -0300

    fix: ts cypress file

--- cypress/e2e/indexDCard.cy.tsx
+++ cypress/e2e/indexDCard.cy.tsx
@@ -4,6 +4,10 @@ import DCard from "../../src/components/3DCard/index";
 describe("<DCard />", () => {
 	it("renders", () => {
 		// see: https://on.cypress.io/mounting-react
-		cy.mount(<DCard />);
+		cy.mount(
+			<DCard>
+				<p>aaa</p>
+			</DCard>
+		);
 	});
 });
