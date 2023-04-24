8355da074d06468e3b1db6b9ed167ea9c1e482da
Lucca Paradeda <lucca.paradeda@edu.pucrs.br>
2023-04-24 00:47:28 -0300

    fix: prettier file

---
+++ cypress/e2e/indexDCard.cy.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import DCard from "../../src/components/3DCard/index";
+
+describe("<DCard />", () => {
+	it("renders", () => {
+		// see: https://on.cypress.io/mounting-react
+		cy.mount(<DCard />);
+	});
+});
--- src/components/3DCard/indexDCard.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import DCard from './index'
-
-describe('<DCard />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<DCard />)
-  })
-})
